a49ef93764451365f95344460ebcf8c68760fdf1
Ruslan <rk.box@mail.ru>
2022-01-26 16:10:00 +0300

    з1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	byte a = 127;
+    short b = 200;
+    int c = 4000;
+    long d = 10000000001L;
+
+    float e = 3.5f;
+    double f = 4.5;
+
+    char g = 56;
+
+    boolean h = true;
+// з1
+
+
     }
 }

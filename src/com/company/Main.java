5b439d8bdc2d3a21890a7fdf52acafabff6aec0d
Ruslan <rk.box@mail.ru>
2022-01-31 14:15:21 +0300

    дз №1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,9 +54,19 @@ public class Main {
     float denis = 83690;
     float kris = 76230;
 
-    int percent = 10;
+    float percent = 10f;
+    float coff = 1f + (percent / 100);
 
+    float mashaNew = masha * coff;
+    float denisNew = denis * coff;
+    float krisNew = kris * coff;
 
+    float mashaDiff = (mashaNew * 12) - (masha * 12);
+    float denisDiff = (denisNew * 12) - (denis * 12);
+    float krisDiff = (krisNew * 12) - (kris * 12);
 
+    System.out.println("з5 Маша теперь получает " + mashaNew + ", Годовой доход вырос на " + mashaDiff);
+    System.out.println("Денис теперь получает " + denisNew + ", Годовой доход вырос на " + denisDiff);
+    System.out.println("Кристина теперь получает " + krisNew + ", Годовой доход вырос на " + krisDiff);
     }
 }
